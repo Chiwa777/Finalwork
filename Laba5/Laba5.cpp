29a8621dc4fc5a24c1ea32ecc6564ce80628b809
Arseny <tigrtv64@gmail.com>
2025-04-16 09:42:30 +0300

    fspoefk

---
+++ Laba5/Laba5.cpp
@@ -0,0 +1,40 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+
+double factorial(int n) {
+    if (n == 0) return 1;
+    double result = 1;
+    for (int i = 1; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
+
+double computeCos(double x) {
+    double sum = 1.0;
+    double term;
+    int n = 1;
+
+    do {
+        term = pow(-1, n) * pow(x, 2 * n) / factorial(2 * n);
+        sum += term;
+        n++;
+    } while (fabs(term) >= 0.001);
+
+    return sum;
+}
+
+int main() {
+    double x;
+    setlocale(LC_ALL, "");
+
+    printf("Введите значение x (в радианах): ");
+    scanf("%lf", &x);
+
+    double cosX = computeCos(x);
+    printf("Cos(%.2f) = %.6f\n", x, cosX);
+
+    return 0;
+}
