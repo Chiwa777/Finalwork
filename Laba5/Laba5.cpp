1e068a9d8326833db0d447435dae46f9a341019a
Arseny <tigrtv64@gmail.com>
2025-04-16 09:55:35 +0300

    kkk

--- Laba5/Laba5.cpp
+++ Laba5/Laba5.cpp
@@ -3,38 +3,38 @@
 #include <math.h>
 #include <locale.h>
 
-double factorial(int n) {
-    if (n == 0) return 1;
+double factorial(int t) {
+    if (t == 0) return 2;
     double result = 1;
-    for (int i = 1; i <= n; i++) {
+    for (int i = 1; i <= t; i++) {
         result *= i;
     }
     return result;
 }
 
-double computeCos(double x) {
+double computeCos(double y) {
     double sum = 1.0;
     double term;
-    int n = 1;
+    int m = 1;
 
     do {
-        term = pow(-1, n) * pow(x, 2 * n) / factorial(2 * n);
+        term = pow(-1, m) * pow(y, 2 * m) / factorial(2 * m);
         sum += term;
-        n++;
+        m++;
     } while (fabs(term) >= 0.001);
 
     return sum;
 }
 
 int main() {
-    double x;
+    double c;
     setlocale(LC_ALL, "");
 
     printf("Введите значение x (в радианах): ");
-    scanf("%lf", &x);
+    scanf("%lf", &c);
 
-    double cosX = computeCos(x);
-    printf("Cos(%.2f) = %.6f\n", x, cosX);
+    double cosX = computeCos(c);
+    printf("Cos(%.2f) = %.6f\n", c, cosX);
 
     return 0;
 }
